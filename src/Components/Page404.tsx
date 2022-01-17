888d9c13a2ce656c49c01ceec22c3edb02daace1
karan bohara <karanrockbohara@gmail.com>
2022-01-17 11:59:40 +0530

    feat:more pages updated

--- src/Components/Page404.tsx
+++ src/Components/Page404.tsx
@@ -6,18 +6,18 @@ import "./Page404.css";
 const Page404 = () => {
   return (
     <div className="errorpage">
-     <div className="image-error"><img src={Error} alt="404 page" width="80%" height="100%"></img></div>
+     <div className="image-error"><img src={Error} alt="404 page" ></img></div>
      <div className="error-content">
        <h2>OOPS! PAGE NOT FOUND</h2>
-       <p style={{fontSize:"1.4rem",width:"50%"}}>
+       <p>
        The page you were looking for could not be found.It might have been removed 
        ,renamed or did not exist.
-        <button className="Page404">
+      </p> 
+      <button className="Page404">
           <Link to="/" className="link-remove">
             Home{" "}
           </Link>
         </button>
-      </p> 
        </div>
     </div>
   );
